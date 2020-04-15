0cff94b1d0d4f47aa4f0a929556acaf6e6e20a80
Matt J <MacITMatt@users.noreply.github.com>
2020-04-15 15:06:49 -0700

    Update notification.lua
    Updated to use HTTPS

--- notification.lua
+++ notification.lua
@@ -2,10 +2,5 @@
 -- https://github.com/MacITMatt/Vera-Pushover-notification
 -- replace <App token> & <User Token> with tokens from your pushover account
 
-  local http = require ( "socket.http" )
- http .  TIMEOUT = 5
-
- result , status = http .  request ( "https://api.pushover.net/1/messages.json" , "token=<App token> & user=<User Token> & message=Garage Up  & title=Garage  & priority=1" )
-
- - luup .  log ( result ) 
-
+  local https = require "ssl.https"
+local status = https.request ( "https://api.pushover.net/1/messages.json" , "token=<App token> & user=<User Token> & message=Garage Up  & title=Garage  & priority=1" )return true
